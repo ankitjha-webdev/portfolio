f04200fa0e6c3575b463b1451cfdd583c9a44391
ankitjha-webdev <190101120091@cutm.ac.in>
2024-03-14 00:54:12 +0530

    File changes

--- components/topbar/Topbar.jsx
+++ components/topbar/Topbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { AiOutlineHome } from 'react-icons/ai';
 import { AiOutlineUser } from 'react-icons/ai';
 import { BiBook } from 'react-icons/bi';
--- server/index.js
+++ server/index.js
@@ -68,7 +68,7 @@ async function startServer() {
     }
   })
 
-  const port = process.env.PORT || 3000
+  const port = process.env.PORT || 3100
   app.listen(port)
   console.log(`Server running at http://localhost:${port}`)
 }
